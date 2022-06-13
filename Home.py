f11c5adaa2b207e84e0a3cfee102af4c697086eb
Carole <carole.darve@gmail.com>
2022-06-13 16:42:19 -0700

    works like i want it to i think

--- Home.py
+++ Home.py
@@ -2,19 +2,28 @@ import os
 
 with open('pdfInfo.txt', 'w') as text:
     # count = 0
+    listOfIDs = []
+    allText = []
     for filename in os.listdir("PDFs"):
         # print(filename)
         f = os.path.join("PDFs", filename)
         if os.path.isfile(f):
 
             with open(f, encoding="utf8") as pdf:
-                # count += 1
-                text.write(filename + "\n")
-                # prints ids per file
+                newPDF = [filename]
                 lines = pdf.read()
                 indexOfOSMSC = lines.find("OSMSC")
                 indexOfNewLine = lines.find("\n", indexOfOSMSC)
                 idsOfPdf = lines[indexOfOSMSC + 5:indexOfNewLine].split()
                 for id in idsOfPdf:
-                    text.write("\t" + id + "\n")
-    # text.write(str(count))
+                    if id not in allText:
+                        listOfIDs.append(id)
+                        allText.append(id)
+                        allText.append(filename)
+                    else:
+                        allText.insert(allText.index(id) + 1, filename)
+    for line in allText:
+        if line in listOfIDs:
+            text.write(line + "\n")
+        else:
+            text.write("\t" + line + "\n")
