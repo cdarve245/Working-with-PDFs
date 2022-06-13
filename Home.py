3f4673586a3777382c74d6853b42f0e636cf730e
Carole <carole.darve@gmail.com>
2022-06-12 22:58:46 -0700

    issue: not all files present

--- Home.py
+++ Home.py
@@ -1,12 +1,14 @@
 import os
 
 with open('pdfInfo.txt', 'w') as text:
+    # count = 0
     for filename in os.listdir("PDFs"):
         # print(filename)
         f = os.path.join("PDFs", filename)
         if os.path.isfile(f):
 
             with open(f, encoding="utf8") as pdf:
+                # count += 1
                 text.write(filename + "\n")
                 # prints ids per file
                 lines = pdf.read()
@@ -15,3 +17,4 @@ with open('pdfInfo.txt', 'w') as text:
                 idsOfPdf = lines[indexOfOSMSC + 5:indexOfNewLine].split()
                 for id in idsOfPdf:
                     text.write("\t" + id + "\n")
+    # text.write(str(count))
