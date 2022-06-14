a8a5d0f8c81b3d47cfe8c5d6f868773085d15847
Carole <carole.darve@gmail.com>
2022-06-13 18:04:00 -0700

    all pdfs now here

--- Home.py
+++ Home.py
@@ -1,29 +1,41 @@
 import os
 
 with open('pdfInfo.txt', 'w') as text:
-    # count = 0
-    listOfIDs = []
+    pdfInfo = []
+    listOfIDs = set()
     allText = []
+    allTitles = []
     for filename in os.listdir("PDFs"):
-        # print(filename)
         f = os.path.join("PDFs", filename)
         if os.path.isfile(f):
 
             with open(f, encoding="utf8") as pdf:
                 newPDF = [filename]
+
                 lines = pdf.read()
+
                 indexOfOSMSC = lines.find("OSMSC")
                 indexOfNewLine = lines.find("\n", indexOfOSMSC)
                 idsOfPdf = lines[indexOfOSMSC + 5:indexOfNewLine].split()
+
                 for id in idsOfPdf:
-                    if id not in allText:
-                        listOfIDs.append(id)
-                        allText.append(id)
-                        allText.append(filename)
-                    else:
-                        allText.insert(allText.index(id) + 1, filename)
-    for line in allText:
-        if line in listOfIDs:
-            text.write(line + "\n")
-        else:
-            text.write("\t" + line + "\n")
+                    newPDF.append(id)
+                    listOfIDs.add(id)
+
+                searchFor = len("CPM Specifications Document")
+                indexOfTitle = lines.find("CPM Specifications Document")
+                indexOfNewLine = lines.find(":", indexOfTitle)
+                title = lines[indexOfTitle + searchFor:indexOfNewLine]
+
+                newPDF.append(title)
+                pdfInfo.append(newPDF)
+
+    for pdf in pdfInfo:
+        text.write("\n" + pdf[0])
+        text.write("\n" + pdf[len(pdf) - 1] + "\n")
+        for value in pdf:
+            if value in listOfIDs:
+                text.write("\t" + value + "\n")
+        text.write("NEW PDF \n")
+    print(len(listOfIDs))
+    print(len(pdfInfo))
