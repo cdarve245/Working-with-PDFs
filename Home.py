0f3481dd0f53c0d5ec404d01fe4e5392eecbe97e
Carole <carole.darve@gmail.com>
2022-06-13 18:50:10 -0700

    working1

--- Home.py
+++ Home.py
@@ -14,28 +14,55 @@ with open('pdfInfo.txt', 'w') as text:
 
                 lines = pdf.read()
 
+                # find title
+                searchFor = len("CPM Specifications Document")
+                indexOfTitle = lines.find("CPM Specifications Document")
+                indexOfNewLine = lines.find(":", indexOfTitle)
+                title = lines[indexOfTitle + searchFor:indexOfNewLine]
+                newPDF.append(title)
+
+                # find ids
                 indexOfOSMSC = lines.find("OSMSC")
                 indexOfNewLine = lines.find("\n", indexOfOSMSC)
                 idsOfPdf = lines[indexOfOSMSC + 5:indexOfNewLine].split()
 
                 for id in idsOfPdf:
-                    newPDF.append(id)
                     listOfIDs.add(id)
+                    newPDF.append(id)
 
-                searchFor = len("CPM Specifications Document")
-                indexOfTitle = lines.find("CPM Specifications Document")
-                indexOfNewLine = lines.find(":", indexOfTitle)
-                title = lines[indexOfTitle + searchFor:indexOfNewLine]
+                    firstPartID = id[0:4]
+                    secondPartID = id[5:9]
+                    searchFor = len("ID: OSMSC" + firstPartID +
+                                    "\nsubID: " + secondPartID + "\n Age: ")
+                    indexOfAge = lines.find(
+                        "ID: OSMSC" + firstPartID + "\nsubID: " + secondPartID + "\nAge: ")
+                    indexOfNewLine = lines.find("\nGe", indexOfAge + searchFor)
+                    if indexOfAge != - 1 and indexOfNewLine != -1:
+                        age = lines[indexOfAge +
+                                    searchFor - 2:indexOfNewLine]
+                    newPDF.append("Age: " + str(age))
 
+                    searchFor = len("ID: OSMSC" + firstPartID + "\nsubID: " +
+                                    secondPartID + "\nAge: " + str(age) + "\nGender: ")
+                    indexOfGender = lines.find(
+                        "ID: OSMSC" + firstPartID + "\nsubID: " + secondPartID + "\nAge: " + str(age) + "\nGender: ")
+                    indexOfNewLine = lines.find("\n", indexOfAge + searchFor)
+                    if indexOfAge != - 1 and indexOfNewLine != -1:
+                        gender = lines[indexOfAge +
+                                       searchFor - 2:indexOfNewLine]
+                    newPDF.append("Gender: " + gender)
                 newPDF.append(title)
+
                 pdfInfo.append(newPDF)
 
     for pdf in pdfInfo:
         text.write("\n" + pdf[0])
-        text.write("\n" + pdf[len(pdf) - 1] + "\n")
+        text.write("\n" + pdf[1] + "\n")
         for value in pdf:
             if value in listOfIDs:
                 text.write("\t" + value + "\n")
+            if "Age: " in value:
+                text.write("\t" + value + "\n")
+            if "Gender: " in value:
+                text.write("\t" + value + "\n")
         text.write("NEW PDF \n")
-    print(len(listOfIDs))
-    print(len(pdfInfo))
